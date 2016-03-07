77946d34eddc0dacfd8a60a6d275707b30f8d4ed
makpande <mak_pande@yahoo.com>
2016-03-07 11:37:27 -0500

    assignment play pause part 2

--- scripts/album.js
+++ scripts/album.js
@@ -126,3 +126,11 @@ window.onload = function() {
 });
     
 };
+
+var findParentByClassName = function(element, targetClass) {
+    var currentParent = element.parentElement;
+    while (currentParent.className != targetClass) {
+        currentParent = currentParent.parentElement;
+    }
+    return currentParent;
+};
